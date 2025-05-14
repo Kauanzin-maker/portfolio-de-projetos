7f9292a0146000519a519fc5cb385aa562adf8dd
Kauanzin-maker <kauan.carlos.alves@escola.pr.gov.br>
2025-05-14 08:06:40 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Everson',
+    'Quem é Everson',
+    'O Everson é um professor de programação muito bom'
+)
+
+criaCartao(
+    'Programação',
+    'Por que fazer programação?',
+    'Por que a IA vai dominar o mundo'
+)
+
+criaCartao(
+    'Inglês',
+    'O que é inglês',
+    'Um idioma né'
+)
+
+criaCartao(
+    'Inglês',
+    'Como se diz gigante em Inglês?',
+    'Oi em ingles giant'
+)
